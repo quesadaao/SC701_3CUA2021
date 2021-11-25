19491f3b1832296be5361014f13500c622f9a5bf
Oscar.Quesada <quesadaao@hotmail.com>
2021-11-24 21:24:16 -0600

    codigo front end

---
+++ CodeP2/VisualStudio/FrontEnd/UI/Models/Groups.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UI.Models
+{
+    public partial class Groups
+    {
+        public Groups()
+        {
+            //Foci = new HashSet<Foci>();
+        }
+
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        //public virtual ICollection<Foci> Foci { get; set; }
+    }
+}
